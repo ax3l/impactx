99940f91a19c4072c1c9518582bccacd20798ab4
ax3l <1353258+ax3l@users.noreply.github.com>
2026-08-03 23:52:43 +0000

    Update Stub Files

--- src/python/impactx/impactx_pybind/__init__.pyi
+++ src/python/impactx/impactx_pybind/__init__.pyi
@@ -1102,7 +1102,7 @@ class Config:
     have_omp: typing.ClassVar[bool] = True
     have_openpmd: typing.ClassVar[bool] = True
     have_simd: typing.ClassVar[bool] = False
-    impactx_version: typing.ClassVar[str] = "6271b3bb9ee2"
+    impactx_version: typing.ClassVar[str] = "55435ea5d132"
     openpmd_backends: typing.ClassVar[dict] = {
         "adios1": False,
         "adios2": False,
